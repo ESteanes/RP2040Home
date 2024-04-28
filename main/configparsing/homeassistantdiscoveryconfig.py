b11efc04716547311dfd96b8a3a966013ad742e9
skellsrs <skellsrsmail@gmail.com>
2024-04-28 19:13:49 +1000

    Simplifying config

--- main/configparsing/homeassistantdiscoveryconfig.py
+++ main/configparsing/homeassistantdiscoveryconfig.py
@@ -1,8 +1,7 @@
 class HomeAssistantDiscoveryConfig:
     enabled: str
-    discovery_topic_prefix: str
+    node_id: str
     
-    def __init__(self, enabled:str, discovery_topic_prefix: str, node_id: str):
+    def __init__(self, enabled:str, node_id: str):
         self.enabled = enabled
-        self.discovery_topic_prefix = discovery_topic_prefix
         self.node_id = node_id
