ffd2d9887bb440fbe0609b57f649d3379ea9caa6
skellsrs <19161310+ESteanes@users.noreply.github.com>
2024-06-23 16:02:06 +1000

    10 minute timer to refresh connections & minor refactor

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from RP2040Home.RP2040Home import RP2040Home
 
 
 def main():
-    RP2040Home(ConfigParser().load("config.json")).connect_wlan().start_connection()
+    RP2040Home(
+        ConfigParser().load("config.json")
+        ).connect_wlan() \
+        .start_connection() \
+        .subscribe()
 
 main()
